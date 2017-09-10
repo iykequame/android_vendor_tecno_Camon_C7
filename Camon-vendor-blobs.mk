0b74d6b7154b536bfcba43de7829f0e3cbb776d4
iykequame <zac6ix@gmail.com>
2017-09-10 11:03:59 +0000

    test_blobs_for_O

---
+++ Camon-vendor-blobs.mk
@@ -0,0 +1,15 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon/proprietary/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon/proprietary/projekt1,system/projekt1)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon/proprietary/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tecno/Camon/proprietary/vendor,system/vendor)
