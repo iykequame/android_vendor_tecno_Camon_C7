47f5d19f25f972bcefc1f5251b857fd1f4055f61
iykequame <zac6ix@gmail.com>
2017-09-16 20:41:41 +0000

    fix path

--- Android.mk
+++ Android.mk
@@ -1,11 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),Camon)
+ifeq ($(TARGET_DEVICE),Camon_C7)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libhwm
-LOCAL_SRC_FILES_64 := lib64/libhwm.so
-LOCAL_SRC_FILES_32 := lib/libhwm.so
+LOCAL_SRC_FILES_64 := proprietary/lib64/libhwm.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libhwm.so
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -13,8 +13,8 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libcustom_prop
-LOCAL_SRC_FILES_64 := lib64/libcustom_prop.so
-LOCAL_SRC_FILES_32 := lib/libcustom_prop.so
+LOCAL_SRC_FILES_64 := proprietary/lib64/libcustom_prop.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libcustom_prop.so
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -22,8 +22,8 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libdpframework
-LOCAL_SRC_FILES_64 := lib64/libdpframework.so
-LOCAL_SRC_FILES_32 := lib/libdpframework.so
+LOCAL_SRC_FILES_64 := proprietary/lib64/libdpframework.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libdpframework.so
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
